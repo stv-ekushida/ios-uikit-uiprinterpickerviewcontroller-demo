08b59d842fb39f8d004c60a7ab1a0f50b59c884c
stv-ekushida <e.kushida@st-ventures.jp>
2017-03-14 14:23:06 +0900

    ios-uikit-uiprinterpickerviewcontroller-demo

--- ios-uikit-uiprinterpickercontroller-demo/ViewController.swift
+++ ios-uikit-uiprinterpickercontroller-demo/ViewController.swift
@@ -12,14 +12,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didTapShowPrinter(_ sender: UIButton) {
+        showPrinterPicker()
     }
 
+    // UIPrinterPickerControllerをモーダル表示する
+    func showPrinterPicker() {
+        
+        let printerPicker = UIPrinterPickerController(initiallySelectedPrinter: nil)
+        printerPicker.present(animated: true, completionHandler: {
+            
+            [unowned self] printerPickerController, userDidSelect, error in
+            
+            if (error != nil) {
+                print("Error : \(error)")
+            } else {
+                // 選択したUIPrinterを取得する
+                if let printer: UIPrinter = printerPickerController.selectedPrinter {
+                    print("Printer : \(printer.displayName)")
+                    self.printToPrinter(printer: printer)
+                } else {
+                    print("Printer is not selected")
+                }
+            }
+        })
+    }
 
+    // 印刷してみる
+    func printToPrinter(printer: UIPrinter) {
+        let printIntaractionController = UIPrintInteractionController.shared
+        let info = UIPrintInfo(dictionary: nil)
+        info.jobName = "Sample Print"
+        info.orientation = .portrait
+        printIntaractionController.printInfo = info
+        printIntaractionController.printingItem = UIImage(named: "sample.jpg")
+        printIntaractionController.print(to: printer, completionHandler: {
+            controller, completed, error in
+            
+        })
+    }
 }
 
